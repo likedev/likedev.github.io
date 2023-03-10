6c3575cf3fac6d3fd11b6a371dc816a126fefb88
皮皮虾 <2190450230@qq.com>
2023-03-10 10:37:39 +0800

    add gh_pages action and index.html

--- tools/build.go
+++ tools/build.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
+
+const index_html_path = "assets/pages/index.html"
+
+func GetFileName(path string) string {
+	delg := "\\"
+	if strings.Contains(path, "/") {
+		delg = "/"
+	}
+	lastIndex := strings.LastIndex(path, delg)
+	if lastIndex < 0 {
+		return path
+	}
+	return path[lastIndex+1:]
+}
+
+func CopyToOut(path string, dst_dir string) {
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		os.Exit(2)
+	}
+	// Write data to dst
+	err = ioutil.WriteFile(dst_dir, data, 0644)
+	if err != nil {
+		os.Exit(2)
+	}
+}
 
 func main() {
-	fmt.Print("build")
+	CopyToOut("./"+index_html_path, "./output/"+GetFileName(index_html_path))
+	fmt.Print("build success")
 }
