76ef67a625905187b85ad9071b80eadb81a1f130
皮皮虾 <2190450230@qq.com>
2023-03-10 10:51:00 +0800

    fix build file

--- tools/build.go
+++ tools/build.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 )
@@ -24,16 +25,25 @@ func GetFileName(path string) string {
 func CopyToOut(path string, dst_dir string) {
 	data, err := ioutil.ReadFile(path)
 	if err != nil {
+		fmt.Printf("读取文件错误： %s ", path)
 		os.Exit(2)
 	}
 	// Write data to dst
 	err = ioutil.WriteFile(dst_dir, data, 0644)
 	if err != nil {
+		fmt.Printf("写文件错误 %s", dst_dir)
 		os.Exit(2)
 	}
 }
 
 func main() {
+	if _, err := os.Stat("./output"); os.IsNotExist(err) {
+		err := os.Mkdir("./output", 0755)
+		if err != nil {
+			fmt.Printf("创建output文件夹失败")
+			log.Fatal(err)
+		}
+	}
 	CopyToOut("./"+index_html_path, "./output/"+GetFileName(index_html_path))
 	fmt.Print("build success")
 }
